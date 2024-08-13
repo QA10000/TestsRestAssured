df3e08e1c67f47054ffce5865b1e5618d90bbbbf
QA10000 <nsh.klr1@gmail.com>
2024-08-13 09:40:50 -0500

    Added more tests

---
+++ src/test/java/com/qa/reqres/in/BDDgetApiReqres.java
@@ -0,0 +1,59 @@
+package com.qa.reqres.in;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class BDDgetApiReqres {
+    @Test(priority = 1)
+    public void getAllUsers() {
+        RestAssured.baseURI = "https://reqres.in/";
+        given().log().all().contentType("application/json")
+                .when().get("api/users?page=2")
+                .then().statusCode(200)
+                .header("server", "cloudflare")
+                .header("Content-Encoding", "gzip");
+    }
+
+    @Test(priority = 2)
+    public void getSingleUser() {
+        RestAssured.baseURI = "https://reqres.in";
+        given().log().all().contentType("application/json")
+                .when().get("/api/users/2")
+                .then().statusCode(200);
+    }
+
+    @Test(priority = 3)
+    public void getUserNotFound() {
+        RestAssured.baseURI = "https://reqres.in";
+        given().log().all().contentType("application/json")
+                .when().get("/api/users/23")
+                .then().statusCode(404);
+
+    }
+
+    @Test(priority = 4)
+    public void getlistResourcss() {
+        RestAssured.baseURI = "https://reqres.in";
+        given().log().all().contentType("application/json")
+                .when().get("/api/unknown")
+                .then().statusCode(200);
+    }
+
+    @Test(priority = 5)
+    public void getSingleResource() {
+        RestAssured.baseURI = "https://reqres.in";
+        given().log().all().contentType("application/json")
+                .when().get("/api/unknown/2")
+                .then().statusCode(200);
+    }
+    @Test(priority = 6)
+    public void getResourcesNotFound() {
+       RestAssured.baseURI = "https://reqres.in";
+       given().log().all().contentType("application/json")
+               .when().get("/api/unknown/23")
+               .then().statusCode(404);
+    }
+
+}
---
+++ src/test/java/com/qa/restassured/BDDgetApis.java
@@ -0,0 +1,20 @@
+package com.qa.restassured;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class BDDgetApis {
+    @Test
+    public void getAllUsers() {
+        RestAssured.baseURI = "https://gorest.co.in";
+        given().contentType("application/json")
+        .when().get("/public/v2/users")
+        .then().statusCode(200)
+        .header("server", "cloudflare")
+                .header("Content-Encoding", "gzip");
+
+
+    }
+}
