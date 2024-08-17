5bd3ab2a3c07e947252c239ba78aba650b78b34d
QA10000 <nsh.klr1@gmail.com>
2024-08-17 08:08:06 -0500

    Added more tests

---
+++ src/test/java/com/qa/restassured/NonBDDGetApis/GoRestApi.java
@@ -0,0 +1,110 @@
+package com.qa.restassured.NonBDDGetApis;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class GoRestApi {
+    @Test
+    public void getUsers() {
+        RestAssured.baseURI = "https://gorest.co.in/";
+        RequestSpecification request = given().log().all();
+        Response response = request.get("/public/v2/users");
+        System.out.println("=========ResponseSection============");
+        //System.out.println("Response Body  \n"+ response.getBody().asString());
+        System.out.println(response.prettyPrint());
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        System.out.println("======ValidateStatusLine======");
+        String statusLine = response.getStatusLine();
+        System.out.println("statusLine " + statusLine);
+        Assert.assertTrue(statusLine.contains("200 OK"));
+        System.out.println("=========GetSingleHeader============");
+        String dateHeader = response.header("Date");
+        Assert.assertTrue(dateHeader.contains("GMT"));
+        System.out.println(dateHeader);
+        Headers headers = response.getHeaders();
+        System.out.println("Print all headers" + headers);
+    }
+    @Test
+    public void getAllInactiveUsers() {
+        System.out.println("=========RequestSection============");
+        RestAssured.baseURI = "https://gorest.co.in/";
+        RequestSpecification request = given().log().all();
+        request.queryParam("status", "inactive");
+        System.out.println("=========ResponseSection============");
+        Response response = request.get("/public/v2/users");
+        System.out.println(response.prettyPrint());
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        System.out.println(statusCode);
+        String statusLine = response.getStatusLine();
+        Assert.assertTrue(statusLine.contains("200 OK"));
+    }
+    @Test
+    public void getAllPosts() {
+        RestAssured.baseURI = "https://gorest.co.in/";
+        RequestSpecification request = given().log().all();
+        Response response = request.get("/public/v2/posts");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        System.out.println("======ValidateStatusLine======");
+        String statusLine = response.getStatusLine();
+        System.out.println("statusLine " + statusLine);
+        Assert.assertTrue(statusLine.contains("200 OK"));
+        System.out.println("=========GetSingleHeader============");
+        String dateHeader = response.header("Date");
+        Assert.assertTrue(dateHeader.contains("GMT"));
+        System.out.println(dateHeader);
+        Headers headers = response.getHeaders();
+        System.out.println("Print all headers" + headers);
+    }
+    @Test
+    public void getAllComments() {
+        RestAssured.baseURI = "https://gorest.co.in/";
+        RequestSpecification request = given().log().all();
+        Response response = request.get("/public/v2/comments");
+        System.out.println("=========ResponseSection============");
+        //System.out.println("Response Body  \n"+ response.getBody().asString());
+        System.out.println(response.prettyPrint());
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        System.out.println("======ValidateStatusLine======");
+        String statusLine = response.getStatusLine();
+        System.out.println("statusLine " + statusLine);
+        Assert.assertTrue(statusLine.contains("200 OK"));
+        System.out.println("=========GetSingleHeader============");
+        String dateHeader = response.header("Date");
+        Assert.assertTrue(dateHeader.contains("GMT"));
+        System.out.println(dateHeader);
+        Headers headers = response.getHeaders();
+        System.out.println("Print all headers" + headers);
+
+    }
+    @Test
+    public void getAllTodos() {
+        RestAssured.baseURI = "https://gorest.co.in/";
+        RequestSpecification request = given().log().all();
+        Response response = request.get("/public/v2/todos");
+        System.out.println("=========ResponseSection============");
+        //System.out.println("Response Body  \n"+ response.getBody().asString());
+        System.out.println(response.prettyPrint());
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        System.out.println("======ValidateStatusLine======");
+        String statusLine = response.getStatusLine();
+        System.out.println("statusLine " + statusLine);
+        Assert.assertTrue(statusLine.contains("200 OK"));
+        System.out.println("=========GetSingleHeader============");
+        String dateHeader = response.header("Date");
+        Assert.assertTrue(dateHeader.contains("GMT"));
+        System.out.println(dateHeader);
+        Headers headers = response.getHeaders();
+        System.out.println("Print all headers" + headers);
+    }
+}
